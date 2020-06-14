474b0a417de89d3474bc7912b301e03533545f29
Linoy Davari <linoydavari1997@gmail.com>
2020-06-14 15:35:14 +0300

    added screenshot manager as singleton

---
+++ FlightMobileApp/Managers/IScreenshotManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp.Managers
+{
+    public interface IScreenshotManager
+    {
+        public Task GetScreenshot();
+    }
+}
---
+++ FlightMobileApp/Managers/MyScreenshotManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp.Managers
+{
+    public class MyScreenshotManager : IScreenshotManager
+    {
+        public Task GetScreenshot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
