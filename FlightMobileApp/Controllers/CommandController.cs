e4fe88779243d419acf9682dd6fb3455c84e33c7
YaelSim <58948260+YaelSim@users.noreply.github.com>
2020-06-14 15:19:49 +0300

    command controller

--- FlightMobileApp/Controllers/CommandController.cs
+++ FlightMobileApp/Controllers/CommandController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlightMobileApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using FlightMobileApp.Managers;
 
 namespace FlightMobileApp.Controllers
 {
@@ -11,5 +13,22 @@ namespace FlightMobileApp.Controllers
     [ApiController]
     public class CommandController : ControllerBase
     {
+        private readonly ICommandManager commandManager;
+
+        // Constructor
+        public CommandController(ICommandManager command)
+        {
+            this.commandManager = command;
+        }
+
+        // check type of return***************** need to return status**********
+        // POST: /api/command
+        [HttpPost]
+        public ActionResult AddCommand([FromBody] Command command)
+        {
+            // need to check if valid**********
+
+            return Ok();
+        }
     }
 }
