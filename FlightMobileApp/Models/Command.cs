583795be8f41effd93643ce4ded7242e27e4e922
Linoy Davari <linoydavari1997@gmail.com>
2020-06-14 12:37:15 +0300

    added command.cs

---
+++ FlightMobileApp/Models/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp.Models
+{
+    public class Command
+    {
+        double Rudder { get; set; }
+        double Aileron { get; set; }
+        double Elevator { get; set; }
+        double Throttle { get; set; }
+    }
+}
